893d8877d44065e17dd05d579040a24644b790c5
Darren Yin <darrenyin0110@yahoo.com>
2017-12-04 04:00:36 -0800

    modified code in GPR model stuff

--- GPRModel/graphModel.m
+++ GPRModel/graphModel.m
@@ -1,13 +1,21 @@
 
 % file to graph the partitioned gpr model (cross valuated with kfold 5
 % times)
-% graphs validation predictions vs. train set data
+% graphs entirevalidation predictions vs. train set data
+% graphs first and last 5k data points of RW9 vs. Trained Model predictions
 
 %% Instructions
 % load GprModel/data/rw9ReducedTrainSet
 % load GprModel/data/rw9ValidationPrediction
 
+% load GprModel/RW9_rw_table or whichever battery to predict on
+
+% change below to battery being used
+data = rw9Tbl;
+battery = 'RW9';
+
 %% Graph train set vs. validation prediction
+
 x = rw9ReducedTrainSet.RelativeTime/3600;
 
 figure;
@@ -23,21 +31,27 @@ ylabel('Voltage (V)');
 data_points = 5000;
 
 % First 5000 points
-figure;
-plot(x(1:data_points), rw9ReducedTrainSet.Voltage(1:data_points), 'r');
-hold on;
-plot(x(1:data_points), validationPredictions(1:data_points), 'b');
+yfit = GprModel(data(1:data_points,:));
 
-title('First 5k Train Set vs. Validation Prediction');
+figure;
+plot(x(1:data_points), data.Voltage(1:data_points), 'r');
+hold on;
+plot(x(1:data_points), yfit, 'b');
+
+legend(battery, 'Trained Model', 'Location', 'Best');
+title('Train Phase First 5k data points');
 xlabel('Time (h)');
 ylabel('Voltage (V)');
 
 % Last 5000 points
-figure;
-plot(x(end-data_points:end), rw9ReducedTrainSet.Voltage(end-data_points:end), 'r');
-hold on;
-plot(x(end-data_points:end), validationPredictions(end-data_points:end), 'b');
+yfit = GprModel(data(1:data_points,:));
 
-title('Last 5k Train Set vs. Validation Prediction');
+figure;
+plot(x(end-data_points:end), data.Voltage(end-data_points:end), 'r');
+hold on;
+plot(x(end-data_points:end), yfit, 'b');
+
+legend(battery, 'Trained Model', 'Location', 'Best');
+title('Train Phase Last 5k data points');
 xlabel('Time (h)');
 ylabel('Voltage (V)');
